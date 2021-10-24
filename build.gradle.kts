333fe2e4a63e8dd9189836bb12fd427fa294e912
leanderBehr <leandersbehr@gmail.com>
2021-10-24 15:59:34 +0200

    Make debugging support optional and bump version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "org.sleepy"
-version = "0.5"
+version = "0.6"
 
 repositories {
     mavenCentral()
@@ -26,7 +26,7 @@ tasks {
     patchPluginXml {
         changeNotes.set(
             """
-            Performance improvements.
+            Add support for debugging with native debuggers in CLion.
             """.trimIndent()
         )
     }
